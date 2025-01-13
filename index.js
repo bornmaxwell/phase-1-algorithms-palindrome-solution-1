387842fcb745a4075766bd4f9efa506b7d07948d
Maxwell Born <borncmaxwell@gmail.com>
2025-01-13 17:01:05 -0600

    algorithms solution 1

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+
+// Function to reverse a string
+function reverseString(word) {
+  return word.split('').reverse().join('');
 }
 
+// Function to check if a string is a palindrome
+function isPalindrome(word) {
+  // Reverse the input string
+  const reversedWord = reverseString(word);
+  
+  // Compare the reversed string to the original and return the result
+  return word === reversedWord;
+}
+
+// Export the function for testing
+module.exports = isPalindrome;
+
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  1. Reverse the input string using a helper function:
+      - Split the string into an array of characters.
+      - Reverse the array.
+      - Join the reversed array back into a string.
+  2. Compare the reversed string with the original string:
+      - If they are identical, return true.
+      - Otherwise, return false.
 */
 
-/*
-  Add written explanation of your solution here
+/* 
+  Written Explanation:
+  The `isPalindrome` function determines if a string reads the same forwards and backwards. 
+  By reversing the string and comparing it to the original, we can check for this property.
+  The helper function `reverseString` handles the string reversal process.
 */
 
-// You can run `node index.js` to view these console logs
+// Test cases
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
 
-module.exports = isPalindrome;
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+}
